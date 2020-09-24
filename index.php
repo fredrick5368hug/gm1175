e88de331eddbc7a07191f1b5ae0dc134353b2630
fredrick5368hug <57219566+fredrick5368hug@users.noreply.github.com>
2020-09-24 15:40:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.in/s?k=asharfi+fabrics&crid=3OIAF4XM0W1WF&sprefix=asharfi+fab%2Caps%2C438&ref=nb_sb_ss_i_1_11");
+header("Location: https://www.microsoft.com/en-ca/microsoft-365/p/microsoft-365-family/cfq7ttc0k5dm?rtc=1&activetab=pivot%3aoverviewtab");
 die();
 ?>
